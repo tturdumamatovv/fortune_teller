c48c892e6a16117347ae6eee849929b6d1212023
tturdumamatovv <muki2022@mail.ru>
2024-04-05 23:11:20 +0600

    a

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/certificates/'
+MEDIA_URL = ''
 MEDIA_ROOT = os.path.join(BASE_DIR, 'certificates/')
 
 
